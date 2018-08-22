11f3426a057091a4a9015ec48b30d4d636534852
Miah Mohd <miahmohd98@gmail.com>
2018-08-22 13:41:05 +0200

    Legend

--- src/views/D3BubbleGraph.vue
+++ src/views/D3BubbleGraph.vue
@@ -1,58 +1,66 @@
 <template>
-<v-container class="container" fluid="true">
+<div class="container fluid">
   
-        <v-layout row >
-            <v-flex lg12 class="py-2">
-                <v-btn-toggle v-model="viewID" mandatory>
-                    <v-btn flat color="primary" value="default">
-                        dafault
-                    </v-btn>
-                    <v-btn flat color="primary" value="missione">
-                        per missione
-                    </v-btn>
-                </v-btn-toggle>
-            </v-flex>
-         </v-layout >
- 
-        <v-layout class="graph-layout" wrap>
-           
-            <v-flex  class="py-2">
-                <BudgetBubbles @myevent="handle" :partitionID="viewID" />
-            </v-flex>
-           
-        </v-layout >
-
-  </v-container>
-
+    <div class="partition-buttons">
+        <v-btn-toggle v-model="viewID" mandatory>
+            <v-btn flat color="primary" value="default">
+                dafault
+            </v-btn>
+            <v-btn flat color="primary" value="missione">
+                per missione
+            </v-btn>
+        </v-btn-toggle>
+    </div>
+    <BubbleGraphLegend v-if="viewID=='default'" class="legend-layout" />
+    <BudgetBubbles class="graph-layout" @myevent="handle" :partitionID="viewID" />
+     
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
 import BudgetBubbles from "@/components/BudgetBubbles.vue";
+import BubbleGraphLegend from "@/components/BubbleGraphLegend.vue";
 
 export default {
   components: {
-    BudgetBubbles
+    BudgetBubbles,
+    BubbleGraphLegend
   },
 
   data: () => {
     return {
-      viewID: "default",
+      viewID: "default"
     };
   },
-  methods:{
-      handle(d){
-          console.log(d);
-          
-      }
+  methods: {
+    handle(d) {
+      console.log(d);
+    }
   }
 };
 </script>
 
 <style>
-.container{
-    height: 100%;
+.container {
+  display: grid;
+  grid-template-areas:
+    "head head head head head"
+    "legend graph graph graph graph" ;
+  grid-template-rows: 3rem auto;
+  height: 100%;
 }
+.partition-buttons {
+  grid-area: head;
+  height: 36px;
+}
+.graph-layout {
+  grid-area: graph;
+}
+.filters-layout {
+  grid-area: filters;
+}
+
 .container > .graph-layout {
   width: 100%;
   height: 100%;
