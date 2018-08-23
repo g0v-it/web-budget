aed7e19b34791611578e17405ffa6915e93abd2f
leonardo longhi <leonardolonghi64@gmail.com>
2018-08-23 18:21:29 +0200

    addedGraph

---
+++ src/components/GraphBubble.vue
@@ -0,0 +1,106 @@
+<template>
+    <div id="bubble-tooltip"  >
+            <v-card-title id="tooltipTitle" class="headline grey lighten-2" primary-title><p>{{topLevel}}</p></v-card-title>
+             <v-card-text id="tooltipText">
+              <p>{{title}}</p>
+            <!--<h3 class="tooltip">{{mission}}</h3>-->
+            </v-card-text>
+            <div id="numberContainer">
+              
+                <div id="amount"><h3>{{amount}}</h3></div>
+                <div id="diff" :style="{backgroundColor:bgColor}" ><h3>{{diff}}</h3></div>
+               
+            </div>                
+    </div>
+</template>
+<script>
+export default {
+  props: {
+    topLevel: String,
+    title: String,
+    amount: String,
+    diff: String,
+    bgColor: String,
+    dkColor: String,
+    xPos: Number,
+    yPos: Number
+  }
+};
+</script>
+<style>
+#bubble-tooltip {
+  width: 25rem;
+  height: 15rem;
+  position: absolute;
+  z-index: 20;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border-radius: 2px;
+  min-width: 0;
+  text-decoration: none;
+  -webkit-box-shadow: 0 2px 1px -1px rgba(0, 0, 0, 0.2),
+    0 1px 1px 0 rgba(0, 0, 0, 0.14), 0 1px 3px 0 rgba(0, 0, 0, 0.12);
+  box-shadow: 0 2px 1px -1px rgba(0, 0, 0, 0.2), 0 1px 1px 0 rgba(0, 0, 0, 0.14),
+    0 1px 3px 0 rgba(0, 0, 0, 0.12);
+  background-color: #fff;
+  color: rgba(0, 0, 0, 0.87);
+}
+#tooltipTitle {
+  padding: 0rem;
+  text-align: center;
+}
+
+#tooltipTitle p {
+  width: 100%;
+  font-size: 13px;
+  margin: 0px;
+}
+
+#tooltipText {
+  padding: 0rem;
+}
+#tooltipText p {
+  padding-left: 1rem;
+  padding-right: 1rem;
+  padding-top: 0.5rem;
+  padding-bottom: 0.1rem;
+  font-size: 13px;
+  margin: 0rem;
+}
+#numberContainer {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  vertical-align: middle;
+  padding-left: 1rem;
+  padding-right: 1rem;
+}
+
+#numberContainer h3 {
+  display: table-cell;
+  vertical-align: middle;
+}
+#numberContainer #amount {
+  background-color: #fff;
+  padding: 0.5rem;
+  margin-bottom: 0.5rem;
+}
+#numberContainer #diff {
+  width: 8rem;
+  color: #fff;
+  padding: 0.5rem;
+  margin-bottom: 0.5rem;
+  text-align: center;
+  border-radius: 28px;
+   -webkit-box-shadow: 0 2px 1px -1px rgba(0, 0, 0, 0.2),
+    0 1px 1px 0 rgba(0, 0, 0, 0.14), 0 1px 3px 0 rgba(0, 0, 0, 0.12);
+  box-shadow: 0 2px 1px -1px rgba(0, 0, 0, 0.2), 0 1px 1px 0 rgba(0, 0, 0, 0.14),
+    0 1px 3px 0 rgba(0, 0, 0, 0.12);
+}
+#numberContainer #diff h3{
+  width: 8rem;
+}
+
+</style>
+
