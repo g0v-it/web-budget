5186dfb00113a15293f457df9226a17f4553b62e
leonardo longhi <leonardolonghi64@gmail.com>
2018-09-20 00:09:23 +0200

    grafico

--- src/components/GraphBubble.vue
+++ src/components/GraphBubble.vue
@@ -14,35 +14,41 @@ import * as d3 from "d3";
     let height = 420 - 2 * margin;
     let barSeparation=0.4;
     let animationDuration=1000;
-    let min=0;
-    let max=100;
+    let min;
+    let max;
+    let data=[];
     let t;
     let svg;
     let chart;
-    let data=[{year: 2010,value: 10},
-      {year: 2011,value: 20},
-      {year: 2012,value: 30},
-      {year: 2013,value: 40},
-      {year: 2014,value: 50},
-      {year: 2015,value: 60}];
   //---------------------------------------------------------
   //SCALE FUNCTIONS
-    const xScale = d3.scaleBand()
-      .range([0, width])
-      .domain(data.map((s) => s.year))
-      .padding(barSeparation)
-    const yScale = d3.scaleLinear()
-      .range([height, 0])
-      .domain([min, max]);
-  
+    let xScale;
+    let yScale;
+    const optimize= function(input){
+      let output=[]
+      Object.keys(input).forEach(element => {
+      output.push({year:element,value:parseFloat(input[element])/1000000000})
+      });
+      console.log(output);
+      return(output);
+      
+    }
   //----------------------------------------------------------
 export default {
   props: {
       values: Object
   },
-  mounted(){
-      console.log('barre', this.values);
-    //----------------------------------------------------------
+  watch: {
+    values(newVal, oldVal) {
+      if(newVal!={}){
+        data=optimize(this.values)
+        max = d3.max(data, function(d) { return parseFloat(d.value);} );
+        max+=max*5/100
+        min = d3.min(data, function(d) { return parseFloat(d.value);} );
+        min-= min/5*100
+        yScale = d3.scalePow().exponent(1).range([height, 0]).domain([0, max]);
+        xScale = d3.scaleBand().range([0, width]).domain(data.map((s) => s.year)).padding(barSeparation)
+          //----------------------------------------------------------
     //DROW GRAPHIC BOUNDLE
     
     t = d3.transition().duration(animationDuration);
@@ -63,60 +69,53 @@ export default {
       .attr('text-anchor', 'middle')
       .text('Bilions');
     //LABEL Y AXIS
-    svg.append('text')
+     svg.append('text')
       .attr('class', 'label')
       .attr('x', width / 2 + margin)
       .attr('y', height + margin * 1.7)
       .attr('text-anchor', 'middle')
       .text('year');
-    const barGroups = chart.selectAll()
-      .data(data)
-      .enter()
-      .append('g')
+        const barGroups = chart.selectAll()
+        .data(data)
+        .enter()
+        .append('g')
+        
+        barGroups.append('rect')
+          .attr('class', 'bar')
+          .attr('x', (g) => xScale(parseInt(g.year)))
+          .attr('y', (g) => yScale(min))
+          .attr("height", 0)
+          .attr('width', xScale.bandwidth())
+          .on('mouseenter', function (actual, i) {
+          d3.selectAll('.valueText')
+            .attr('opacity', 0)
+          d3.select(this)
+            .transition()
+            .duration(300)
+            .attr('opacity', 0.6)
+            .attr('x', (a) => xScale(a.year) - 5)
+            .attr('width', xScale.bandwidth() + 10)
+          const y = yScale(actual.value)
+          let line = chart.append('line')
+            .attr('id', 'limit')
+            .attr('x1', 0)
+            .attr('y1', y)
+            .attr('x2', width)
+            .attr('y2', y)
 
-    barGroups
-      .append('rect')
-      .attr('class', 'bar')
-      .attr('x', (g) => xScale(g.year))
-      .attr('y', (g) => yScale(0))
-      .attr("height", 0)
-      .attr('width', xScale.bandwidth())
-      .on('mouseenter', function (actual, i) {
-        d3.selectAll('.valueText')
-          .attr('opacity', 0)
-
-        d3.select(this)
-          .transition()
-          .duration(300)
-          .attr('opacity', 0.6)
-          .attr('x', (a) => xScale(a.year) - 5)
-          .attr('width', xScale.bandwidth() + 10)
-
-        const y = yScale(actual.value)
-
-        let line = chart.append('line')
-          .attr('id', 'limit')
-          .attr('x1', 0)
-          .attr('y1', y)
-          .attr('x2', width)
-          .attr('y2', y)
-
-        barGroups.append('text')
-          .attr('class', 'divergence')
-          .attr('x', (a) => xScale(a.year) + xScale.bandwidth() / 2)
-          .attr('y', (a) => yScale(a.value) -30)
-          .attr('fill', 'white')
-          .attr('text-anchor', 'middle')
-          .text((a, idx) => {
-            const divergence = (a.value - actual.value).toFixed(1)
-            
-            let text = ''
-            if (divergence > 0) text += '+'
-            text += `${divergence}%`
-
-            return idx !== i ? text : '';
-          })
-
+          barGroups.append('text')
+            .attr('x', (a) => xScale(a.year) + xScale.bandwidth() / 2)
+            .attr('class', 'divergence')
+            .attr('y', (a) => yScale(a.value) -30)
+            .attr('fill', 'white')
+            .attr('text-anchor', 'middle')
+            .text((a, idx) => {
+              const divergence = (a.value - actual.value).toFixed(1)
+              let text = ''
+              if (divergence > 0) text += '+'
+              text += `${divergence}%`
+              return idx !== i ? text : '';
+            })
       })
       .on('mouseleave', function () {
         d3.selectAll('.valueText')
@@ -132,8 +131,8 @@ export default {
         chart.selectAll('.divergence').remove()
       })
       .transition(t)
-      .attr("height", (g) => (height - yScale(g.value)))
-      .attr('y',(g)=>yScale(g.value));
+      .attr("height", (g) => (height - yScale(parseFloat(g.value))))
+      .attr('y',(g)=>yScale(parseFloat(g.value)));
     //LABEL ON BAR
     barGroups 
       .append('text').attr('class', 'valueText')
@@ -147,12 +146,10 @@ export default {
         const i = d3.interpolateRound(v0, v1);
         
         return t => {this.textContent = i(t)};
-      });
-    
-
-
-
-  }
+      }); 
+      }
+    }
+  },
 };
 </script>
 <style>
