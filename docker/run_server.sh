715d5a1bf38ff8ae20d05f427d5afeb1b7d95af3
admin@e-artspace.com <enrico@linkeddata.center>
2019-05-08 13:08:35 +0200

    Bug fixed in G0V_MAX_RADIUS management

--- docker/run_server.sh
+++ docker/run_server.sh
@@ -93,7 +93,7 @@ fi
 
 if [ "$G0V_MAX_RADIUS" != "" ]
 then
-    G0V_MAX_RADIUS="\"g0vMaxRadius\": \"$G0V_MAX_RADIUS\","
+    G0V_MAX_RADIUS="\"g0vMaxRadius\": $G0V_MAX_RADIUS,"
 fi
 
 cat > /usr/share/nginx/html/config.json <<CONF
